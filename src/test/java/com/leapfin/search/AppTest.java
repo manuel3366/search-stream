d24576177298be6fcc596d6a5b1e5e0d5bb52603
edwin.saucedo <edwin.saucedo>
2022-03-03 17:11:41 -0500

    Add a default test case

--- src/test/java/com/leapfin/search/AppTest.java
+++ src/test/java/com/leapfin/search/AppTest.java
@@ -1,13 +1,34 @@
 package com.leapfin.search;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.leapfin.search.domain.SearchOptions;
+import com.leapfin.search.domain.SearchResult;
+import com.leapfin.search.processor.ParentProcessor;
+import com.leapfin.search.util.SearchResultComparator;
+import com.leapfin.search.util.SearchResultPrinter;
+
+import io.reactivex.Flowable;
 
 public class AppTest {
     
   @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
+  public void runDefaultValues() {
+	  
+	  Integer numberOfProcessors = 10;		
+	  String text = null;
+	  Integer stringLength = null; 
+	  Long timeoutInSeconds = 2L;
+
+
+	  Flowable<SearchResult> resultListFlowable = new ParentProcessor().registerProcessors(new SearchOptions(stringLength, text, timeoutInSeconds), numberOfProcessors);
+	  List<SearchResult> resultList = resultListFlowable.sorted(new SearchResultComparator().reversed()).toList().blockingGet();
+	  SearchResultPrinter.printResults(resultList);
+	  assertEquals(numberOfProcessors, resultList.size());
+
   }
 }
