354cfab6ae13d131ca4d06325dd86151d689a8ca
edwin.saucedo <edwin.saucedo>
2022-03-03 16:35:52 -0500

    Implement logging logic

--- src/main/java/com/leapfin/search/util/ErrorPrinter.java
+++ src/main/java/com/leapfin/search/util/ErrorPrinter.java
@@ -2,17 +2,22 @@ package com.leapfin.search.util;
 
 import java.util.concurrent.TimeoutException;
 
-public class ErrorPrinter {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class ErrorPrinter {
+	
+	private static Logger logger = LogManager.getLogger(ErrorPrinter.class);
+	
 	public static void printError(Throwable e) {
 		if (e instanceof TimeoutException == false) {
-			e.printStackTrace();
+			logger.error(e);
 		}
 		
 	}
 	
 	public static void printWarning(String message) {
-		System.err.println(message);
+		logger.warn(message);
 		
 	}
 }
--- src/main/java/com/leapfin/search/util/SearchResultPrinter.java
+++ src/main/java/com/leapfin/search/util/SearchResultPrinter.java
@@ -3,10 +3,16 @@ package com.leapfin.search.util;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.leapfin.search.domain.SearchResult;
 import com.leapfin.search.domain.SearchResult.SearchStatus;
 
 public class SearchResultPrinter {
+	
+	private static Logger logger = LogManager.getLogger(SearchResultPrinter.class);
+	
 	public static void printResults(List<SearchResult> resultList) {
 		Double averageBytesRead = 0.0;
 		int countSuccessResults = 0;
@@ -15,10 +21,10 @@ public class SearchResultPrinter {
 				averageBytesRead += searchResult.getByteCount() / searchResult.getElapseTime();
 				countSuccessResults++;
 			}			
-			System.out.println(searchResult);
+			logger.info(searchResult);
 		}
 		averageBytesRead = averageBytesRead/countSuccessResults;
-		System.out.println("["+averageBytesRead+"]");
+		logger.info("[{}]", averageBytesRead);
 	}
 	
 }
